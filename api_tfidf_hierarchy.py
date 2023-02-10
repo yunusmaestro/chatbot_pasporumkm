c5e3955cdb8805217879f12f52d44b538f142279
yunusmaestro <yunusmarsetio@gmail.com>
2023-02-10 18:04:30 +0700

    Try CORS

--- api_tfidf_hierarchy.py
+++ api_tfidf_hierarchy.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://pasporumkm.com"])
 
 @app.route("/ask_chatbot", methods=['GET', 'POST'])
 def print_sentences():
